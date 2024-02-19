3112a699635fc8502efe31acc2c75a4b31a35c1c
ARUN KUMAR D <arun14k08@gmail.com>
2024-02-19 22:35:21 +0530

    removed the extra borders

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,10 +3,10 @@ import { HeroImage, Lines } from "../assets";
 
 const Hero = () => {
     return (
-        <div className="bg-hero bg-contain bg-no-repeat flex  flex-1 w-full h-[650px] mt-4 sm:flex-row flex-col items-center justify-center relative border-green-600 border-2 border-solid overflow-hidden">
+        <div className="bg-hero bg-contain bg-no-repeat flex  flex-1 w-full h-[650px] mt-4 sm:flex-row flex-col items-center justify-center relative overflow-hidden">
             {/* <img src={Lines} alt="lines" className="" /> */}
-            <div className="border-blue-600 border-2 border-solid w-full h-full"></div>
-            <div className=" flex justify-center items-center border-red-600 border-2 border-solid w-full h-full">
+            <div className="w-full h-full"></div>
+            <div className=" flex justify-center items-center  w-full h-full">
                 <img
                     src={HeroImage}
                     alt="circle"
